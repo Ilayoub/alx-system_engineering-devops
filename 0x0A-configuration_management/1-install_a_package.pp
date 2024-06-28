5db9c7187e532f174f2126842ae309f31388990e
Ilayoub <jrindou.ayoub@gmail.com>
2024-06-28 13:50:54 -0700

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
+#!/usr/bin/pup
 # Installs flask from pip3
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
