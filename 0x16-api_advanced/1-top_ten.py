d46d60baaf83727c6eb40cd4772a0f1a9fb2400d
Ilayoub <jrindou.ayoub@gmail.com>
2024-08-13 12:45:10 -0700

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
 """
 The function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit
 """
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the top ten hot posts."""
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {'User-Agent': 'Chrome'}
+    """
+    prints the titles of the first 10 hot posts listed for
+    a given subreddit
+    """
+    url = ("https://api.reddit.com/r/{}?sort=hot&limit=10".format(subreddit))
+    headers = {'User-Agent': 'CustomClient/1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    try:
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            data = response.json()
-            hot_posts = data.get('data', {}).get('children', [])
-            for post in hot_posts[:11]:
-                title = post['data']['title']
-                print(title)
-        else:
-            print(None)
-    except requests.RequestException as e:
+    if response.status_code != 200:
+        print(None)
+        return
+    response = response.json()
+    if 'data' in response:
+        for posts in response.get('data').get('children'):
+            print(posts.get('data').get('title'))
+
+    else:
         print(None)
