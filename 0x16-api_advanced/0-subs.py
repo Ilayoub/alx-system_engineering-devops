bf52d877b8a80619b52a51a1c747393904d100ff
Ilayoub <jrindou.ayoub@gmail.com>
2024-08-13 12:16:42 -0700

    The subscribers number

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,13 +8,18 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
+    """Returns the number of subscribers"""
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'Chrome'}
+
+    try:
+        response = requests.get(url, headers=headers)
+
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        return 0
+        find_subscribers = data.get("data", {}).get("subscribers", 0)
+        return find_subscribers
+    except requests.HTTPError as e:
+        if e.response.status_code == 404:
+            return 0
+        else:
+            return 0
